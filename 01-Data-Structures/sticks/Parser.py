ffb8c2b5d9b4f0b81ef471bc8c6e786a0c0712e3
FIRST_NAME LAST_NAME <kreonicz@gmail.com>
2019-06-05 00:49:22 +0300

    utf - 8 added

--- 01-Data-Structures/sticks/Parser.py
+++ 01-Data-Structures/sticks/Parser.py
@@ -1,6 +1,6 @@
 
-jsonFile1 = open("winedata_1.json", "rt")
-jsonFile2 = open("winedata_2.json", "rt")
+jsonFile1 = open("winedata_1.json", "rt", encoding='utf-8')
+jsonFile2 = open("winedata_2.json", "rt", encoding='utf-8')
 
 contents1 = jsonFile1.read().replace("]", "").replace("[", "")
 contents2 = jsonFile2.read().replace("]", "").replace("[", "").replace("{", ", {", 1)
@@ -41,7 +41,7 @@ sortedList = listOfDict
 sortedList.sort(key=lambda x: x['taster_name'])
 sortedList.sort(key=lambda x: x['price'])
 
-output_file3 = open('sortedJson.json', 'w')
+output_file3 = open('sortedJson.json', 'w', encoding='utf-8')
 output_file3.write(str(sortedList).replace("\'", "\"").replace("\" \"", "null").
                    replace("\"price\": 0", "\"price\": null"))
 output_file3.close()
