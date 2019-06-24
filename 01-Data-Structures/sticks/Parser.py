ac7dd1395ebd625d1023a4d83e56ce61f35e369d
FIRST_NAME LAST_NAME <kreonicz@gmail.com>
2019-06-24 20:47:28 +0300

    HW for lecture 02

--- 01-Data-Structures/sticks/Parser.py
+++ 01-Data-Structures/sticks/Parser.py
@@ -1,4 +1,3 @@
-
 jsonFile1 = open("winedata_1.json", "rt", encoding='utf-8')
 jsonFile2 = open("winedata_2.json", "rt", encoding='utf-8')
 
@@ -45,4 +44,43 @@ output_file3 = open('sortedJson.json', 'w', encoding='utf-8')
 output_file3.write(str(sortedList).replace("\'", "\"").replace("\" \"", "null").
                    replace("\"price\": 0", "\"price\": null"))
 output_file3.close()
+
+listOfSelectedWines = []
+listOfWineNames = ["Gew[üu]rztraminer", "Riesling", "Merlot", "Madera", "Tempranillo", "Red Blend"]
+for x in range(5):
+    wine = {"title": listOfWineNames[x]}
+
+    average_price = 0
+    average_score = 0
+    min_price = 0
+    max_price = 0
+    most_common_region = ""
+    most_common_country = ""
+
+    counter = 0
+    sumOfPrices = 0
+    sumOfPoints = 0
+
+    for wineDictionary in sortedList:
+        if wineDictionary["title"] == wine["title"]:
+
+            dictionaryCountries = {wineDictionary["region_1"] : 1}
+            dictionaryRegions = {}
+
+            dictionaryRegions[=
+            wineDictionary["region_2"]
+            currentPrice = int(wineDictionary["price"])
+            counter += 1
+            sumOfPrices += currentPrice
+            average_price = sumOfPrices / counter
+
+            sumOfPoints += int(wineDictionary["points"].replace("\"", ""))
+            average_score = sumOfPoints / counter
+
+            if min_price == 0 or min_price < currentPrice:
+                min_price = currentPrice
+            if max_price == 0 or max_price > currentPrice:
+                max_price = currentPrice
+
+
 
