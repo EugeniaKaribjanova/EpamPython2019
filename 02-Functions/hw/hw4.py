d5d110c3a1a295637b5820c0fcf7489a016ab0c8
FIRST_NAME LAST_NAME <kreonicz@gmail.com>
2019-06-24 22:15:41 +0300

    dann

--- 02-Functions/hw/hw4.py
+++ 02-Functions/hw/hw4.py
@@ -23,41 +23,33 @@
 import inspect
 
 
-def sample_func(*args, **kwargs):
-    print(args, kwargs)
+def sample_func(*targs, **tkwargs):
+    print(targs, tkwargs)
 
 
 def modified_func(func, *fixated_args, **fixated_kwargs):
 
     def new_func(*fixed_args, **fixed_kwargs):
-
         new_args = list(fixated_args+fixed_args)
         for key in fixed_kwargs:
             fixated_kwargs[key] = fixed_kwargs[key]
         fixated_kwargs.update(fixed_kwargs)
         sample_func(new_args, fixated_kwargs)
 
-        """
-        A func implementation of <name_of_func>
-        with pre-applied arguments being:
-        <fixated_args>
-        <fixated_kwargs>
-        source_code:
-        <source_code>
-        """
+    new_func.__name__ = 'func_' + func.__name__
+    source_code = inspect.getsource(new_func)
 
-        new_func.__name__ = 'func_' + func.__name__
-        source_code = inspect.getsource(new_func)
-        new_func.__doc__ = __doc__.replace('<name_of_func>', new_func.__name__)
-        new_func.__doc__ = new_func.__doc__.replace('<fixated_args>', str(fixated_args))
-        new_func.__doc__ = new_func.__doc__.replace('<fixated_kwargs>', str(fixated_kwargs))
-        new_func.__doc__ = new_func.__doc__.replace('<source_code>', source_code)
+    new_func.__doc__ = f'A func implementation of {func.__name__}\n' \
+        f'with pre-applied arguments being:\n' \
+        f'{str(fixated_args), str(fixated_kwargs)}\n' \
+        f'source_code:\n' \
+        f'{source_code}'
+    print(new_func.__doc__)
     return new_func
 
 
 result = modified_func(sample_func, 1, 2, warg1='p', warg2='h', warg3='=', warg4='7')
 result(3, 4, 5, warg4='10', warg5='acid')
-print(result.__doc__)
 
 
 
